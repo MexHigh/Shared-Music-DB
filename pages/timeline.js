afbad2496934c4c23702cf25c4fc4f418529c524
Leon Schmidt <mail@leon.wtf>
2021-04-16 16:50:24 +0200

    Added Timeline functionality

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,7 +1,35 @@
-export default function Timeline() {
+import Track from "../components/Track"
+
+export async function getEdgeProps() {
+    let tracks = JSON.parse(
+        await MDB.get("tracks").catch(e => console.log(e))
+    )
+    return {
+        props: {
+            tracks: tracks
+        },
+        revalidate: 10
+    }
+}
+
+export default function Timeline({ tracks }) {
+    console.log("geth")
     return (
         <div>
-            <h1>in progress</h1>
+            {
+                Object.entries(tracks).reverse().map(([vId, v]) =>
+                    <Track 
+                        key={vId}
+                        url={v.url}
+                        shared_by={v.shared_by}
+                        shared_with={v.shared_with}
+                        shared_at={v.shared_at}
+                        thumbnail={v.thumbnail}
+                        title={v.title}
+                        artist={v.channelTitle}
+                    />
+                )
+            }
         </div>
     )
 }
